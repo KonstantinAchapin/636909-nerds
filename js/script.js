ffffd1a14f0b51d72f0a03fd81ceefe34e28fd30
Konstantin <KonstantinAchapin@mail.ru>
2018-09-06 21:52:06 +0200

    Отступы +

--- js/script.js
+++ js/script.js
@@ -11,48 +11,48 @@ var isStorageSupport = true;
 var storage = "";
 
 try {
-storage = localStorage.getItem("login");
+  storage = localStorage.getItem("login");
 } catch (err) {
-isStorageSupport = false;
+  isStorageSupport = false;
 }
 
-link.addEventListener("click", function (evt) {
-evt.preventDefault();
-popup.classList.add("modal-show");
-
-if (storage) {
-  login.value = storage;
-  password.focus();
-} else {
-  login.focus();
-}
-});
-
-close.addEventListener("click", function (evt) {
-evt.preventDefault();
-popup.classList.remove("modal-show");
-popup.classList.remove("modal-error");
-});
-
-form.addEventListener("submit", function (evt) {
-if (!login.value || !password.value) {
+link.addEventListener("click", function(evt) {
   evt.preventDefault();
+  popup.classList.add("modal-show");
+
+  if (storage) {
+    login.value = storage;
+    password.focus();
+  } else {
+    login.focus();
+  }
+});
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
-  popup.offsetWidth = popup.offsetWidth;
-  popup.classList.add("modal-error");
-} else {
-  if (isStorageSupport) {
-    localStorage.setItem("login", login.value);
-  }
-}
 });
 
-window.addEventListener("keydown", function (evt) {
-if (evt.keyCode === 27) {
-  evt.preventDefault();
-  if (popup.classList.contains("modal-show")) {
-    popup.classList.remove("modal-show");
+form.addEventListener("submit", function(evt) {
+  if (!login.value || !password.value) {
+    evt.preventDefault();
     popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
   }
- }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
 });
